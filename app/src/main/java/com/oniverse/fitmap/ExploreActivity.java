5952705a3beec34014332a72f56b0b4ce3fcc20c
snoumi <steve.noumi@alfa-safety.fr>
2024-04-10 16:07:51 +0200

    REDIRECTION

--- app/src/main/java/com/oniverse/fitmap/ExploreActivity.java
+++ app/src/main/java/com/oniverse/fitmap/ExploreActivity.java
@@ -74,7 +74,7 @@ public class ExploreActivity extends AppCompatActivity {
                 } else if (item.getItemId() == R.id.navigation_explore) {
                     return true;
                 } else if (item.getItemId() == R.id.navigation_chat) {
-                    startActivity(new Intent(ExploreActivity.this, SigninActivity.class));
+                    startActivity(new Intent(ExploreActivity.this, RegisterActivity.class));
                     return true;
                 } else {
                     return false;
