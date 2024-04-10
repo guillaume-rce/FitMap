82156e86b15d0a50d05fb38448df35fcf874953f
snoumi <steve.noumi@alfa-safety.fr>
2024-04-10 16:05:11 +0200

    patch des bugs de dossiers. jaout de la pge de connexion

--- app/src/main/java/com/oniverse/fitmap/RegisterActivity.java
+++ app/src/main/java/com/oniverse/fitmap/RegisterActivity.java
@@ -1,24 +1,87 @@
 package com.oniverse.fitmap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.oniverse.fitmap.SigninActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nameEditText, firstNameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
+    private Button signUpButton;
+    private TextView signInTextView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_register); // Assurez-vous que c'est le bon layout
+
+        nameEditText = findViewById(R.id.nameEditText);
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+        signUpButton = findViewById(R.id.signInButton); // Assurez-vous que l'ID est correct
+        signInTextView = findViewById(R.id.signUpTextView);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString().trim();
+                String firstName = firstNameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty() || name.isEmpty() || firstName.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Les mots de passe ne correspondent pas", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                signUp(email, password, name, firstName);
+            }
         });
+
+        signInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void signUp(String email, String password, String name, String firstName) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign up success, update UI with the signed-in user's information
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        // Vous pouvez rediriger l'utilisateur vers l'activité principale ici
+                        Intent intent = new Intent(RegisterActivity.this, ChatActivity.class);
+                        startActivity(intent);
+                        finish();                     } else {
+                        // If sign up fails, display a message to the user.
+                        Toast.makeText(RegisterActivity.this, "Échec de l'inscription", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/oniverse/fitmap/SigninActivity.java
+++ app/src/main/java/com/oniverse/fitmap/SigninActivity.java
@@ -1,135 +1,62 @@
 package com.oniverse.fitmap;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class SigninActivity extends AppCompatActivity {
 
-    // variable for Firebase Auth
-    private FirebaseAuth mFirebaseAuth;
-
-    // declaring a const int value which we
-    // will be using in Firebase auth.
-    public static final int RC_SIGN_IN = 1;
-
-    // creating an auth listener for our Firebase auth
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-
-    // below is the list which we have created in which
-    // we can add the authentication which we have to
-    // display inside our app.
-    List<AuthUI.IdpConfig> providers = Arrays.asList(
-
-            // below is the line for adding
-            // email and password authentication.
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-
-            // below line is used for adding google
-            // authentication builder in our app.
-            new AuthUI.IdpConfig.GoogleBuilder().build(),
-
-            // below line is used for adding phone
-            // authentication builder in our app.
-            new AuthUI.IdpConfig.PhoneBuilder().build());
+    private EditText emailEditText, passwordEditText;
+    private Button signInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_signin); // Assurez-vous que c'est le bon layout
 
-        // below line is for getting instance
-        // for our firebase auth
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        signInButton = findViewById(R.id.signInButton);
 
-        // below line is used for calling auth listener
-        // for our Firebase authentication.
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @SuppressLint("ResourceType")
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
 
-                // we are calling method for on authentication state changed.
-                // below line is used for getting current user which is
-                // authenticated previously.
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                // checking if the user
-                // is null or not.
-                if (user != null) {
-                    // if the user is already authenticated then we will
-                    // redirect our user to next screen which is our home screen.
-                    // we are redirecting to new screen via an intent.
-                    Intent i = new Intent(SigninActivity.this, TestActivity.class);
-                    startActivity(i);
-                    // we are calling finish method to kill or
-                    // mainactivity which is displaying our login ui.
-                    finish();
-                } else {
-                    // this method is called when our
-                    // user is not authenticated previously.
-                    startActivityForResult(
-                            // below line is used for getting
-                            // our authentication instance.
-                            AuthUI.getInstance()
-                                    // below line is used to
-                                    // create our sign in intent
-                                    .createSignInIntentBuilder()
-
-                                    // below line is used for adding smart
-                                    // lock for our authentication.
-                                    // smart lock is used to check if the user
-                                    // is authentication through different devices.
-                                    // currently we are disabling it.
-                                    .setIsSmartLockEnabled(false)
-
-                                    // we are adding different login providers which
-                                    // we have mentioned above in our list.
-                                    // we can add more providers according to our
-                                    // requirement which are available in firebase.
-                                    .setAvailableProviders(providers)
-
-                                    // below line is for customizing our theme for
-                                    // login screen and set logo method is used for
-                                    // adding logo for our login page.
-                                    .setLogo(R.drawable.profile).setTheme(R.style.Theme)
-
-                                    // after setting our theme and logo
-                                    // we are calling a build() method
-                                    // to build our login screen.
-                                    .build(),
-                            // and lastly we are passing our const
-                            // integer which is declared above.
-                            RC_SIGN_IN
-                    );
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(SigninActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                signIn(email, password);
             }
-        };
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // we are calling our auth
-        // listener method on app resume.
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // here we are calling remove auth
-        // listener method on stop.
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        // Vous pouvez rediriger l'utilisateur vers l'activité principale ici
+                        Toast.makeText(SigninActivity.this, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(SigninActivity.this, "Échec de la connexion", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
